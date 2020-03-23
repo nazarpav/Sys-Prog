cf3d07d9ca258da0a1f9cd1823ea2a9957943cc6
павлюк назар <hasab.k18@gmail.com>
2020-03-23 23:48:34 +0200

    23.03.2020

--- HomeWork/23_03_2020/12_03_2020/MainWindow.xaml.cs
+++ HomeWork/23_03_2020/12_03_2020/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,6 +20,7 @@ namespace _12_03_2020
 {
     public partial class MainWindow : Window
     {
+        Task task1;
         public MainWindow()
         {
             InitializeComponent();
@@ -45,45 +47,69 @@ namespace _12_03_2020
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            int q=0;
-            if(!int.TryParse(QuantityCopies.Text,out q))
+            try
             {
-                System.Windows.MessageBox.Show("Error");
+                int q = 0;
+                if (!int.TryParse(QuantityCopies.Text, out q))
+                {
+                    System.Windows.MessageBox.Show("Error QuantityCopies type");
+                }
+                ProgBar.Maximum = q;
+                task1 = new Task(() => FCopy());
+                task1.Start();
             }
-            Parallel.For(0, q, FCopy);
-            ProgBar.Maximum = q;
+            catch
+            {
+                System.Windows.MessageBox.Show("Eror _step1!");
+            }
         }
-        private void FCopy(int x)
+        private void FCopy()
         {
-            ProgBar = x;
-            File.Copy(Dispatcher.Invoke(()=> From.Text), Dispatcher.Invoke(() => To.Text) + Revers_(GetFileName(x)));
+            for (int i = 0; i < Dispatcher.Invoke(() => (int)ProgBar.Maximum); i++)
+            {
+                Thread.Sleep(500);
+                Dispatcher.Invoke(() => ProgBar.Value++);
+                File.Copy(Dispatcher.Invoke(() => From.Text), Dispatcher.Invoke(() => To.Text) + Revers_(GetFileName()));
+            }
+            EndParallel();
+        }
+        private void EndParallel()
+        {
+            System.Windows.MessageBox.Show("Success!");
+            Dispatcher.Invoke(() => ProgBar.Value = 0);
         }
         private string Revers_(string s)
         {
-            string str=string.Empty;
+            string str = string.Empty;
             foreach (var i in s.Reverse())
             {
                 str += i;
             }
             return str;
         }
-        private string GetFileName(int x)
+        private string GetFileName()
         {
-            Dispatcher.BeginInvoke(new Action(() => ProgBar.Value++));
-            string FName= string.Empty;
+            string FName = string.Empty;
+            Random rnd = new Random();
             foreach (var i in Dispatcher.Invoke(() => From.Text).Reverse())
             {
-                FName += i;
-                if(i == '.')
+                if (i == '.')
                 {
-                    FName += x.ToString();
+                    FName += rnd.Next(int.MinValue, int.MaxValue);
                 }
-                if (i=='/'||i.ToString()==@"\")
+                FName += i;
+                if (i == '/' || i.ToString() == @"\")
                 {
                     return FName;
                 }
             }
             return FName;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            task1.Dispose();
+            Dispatcher.Invoke(() => ProgBar.Value = 0);
         }
     }
 }
